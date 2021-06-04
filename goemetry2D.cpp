9c3720742bfbadcaa4c7f5c2b70a3d558d4da927
ADaA-proj <hzx20000818@gmail.com>
2021-06-05 00:37:26 +0800

    add ConvexHall

--- goemetry2D.cpp
+++ goemetry2D.cpp
@@ -1,10 +1,46 @@
 #include "geometry2D.h"
 #include <cmath>
+#include <algorithm>
 
 static const double eps = 1e-6;
 static inline bool eq(double a, double b)
 {
     return ((a - b) < eps) || ((b - a) < eps);
+}
+
+Polygon ConvexHall(std::vector<Point> point_list) // Graham scan O(nlogn)
+{
+    size_t len = point_list.size();
+    if (len < 3)
+        exit(1);
+
+    auto cmp1 = [](Point &a, Point &b)
+    { return (a.y == b.y) ? (a.x < b.x) : (a.y < b.y); };
+
+    std::sort(point_list.begin(), point_list.end(), cmp1);
+    std::vector<Point> out;
+    out.push_back(point_list[0]);
+
+    auto cmp2 = [&point_list](Point &a, Point &b)
+    { return (atan2(a.y - point_list[0].y, a.x - point_list[0].x) != atan2(b.y - point_list[0].y, b.x - point_list[0].x))
+                 ? (atan2(a.y - point_list[0].y, a.x - point_list[0].x)) < (atan2(b.y - point_list[0].y, b.x - point_list[0].x))
+                 : (a.x < b.x); };
+    std::sort(point_list.begin() + 1, point_list.end(), cmp2);
+
+    out.push_back(point_list[1]);
+    size_t top = 1;
+    for (size_t i = 2; i < len; ++i)
+    {
+        while ((out[top] - out[top - 1]).cross(point_list[i] - out[top]) < 0)
+        {
+            out.pop_back();
+            --top;
+        }
+        out.push_back(point_list[i]);
+        ++top;
+    }
+
+    return Polygon(out);
 }
 
 double Distance(Point p, Line l, int norm = 2)
